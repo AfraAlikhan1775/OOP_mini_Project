c3658253a27a8581ea230ec1ed5809a05ca8f70d
AfraAlikhan1775 <alikhanfathimaafra22@gmail.com>
2026-04-21 15:27:00 +0530

    added

--- src/com/controller/Student/stuDashboardController.java
+++ src/com/controller/Student/stuDashboardController.java
@@ -5,7 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.layout.StackPane;
 
-public class StudentController {
+public class StuDashboardController {
 
     @FXML
     private StackPane contentArea;
